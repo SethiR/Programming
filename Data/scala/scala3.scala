46bb3ab1b22cc0f1eda23675a204ceafd4e47e59
Rajat Sethi <rajat22sethi@gmail.com>
2020-06-04 20:38:30 -0500

    Deployed 7d006e4 with MkDocs version: 1.1.2

---
+++ Data/scala/scala3.scala
@@ -0,0 +1,13 @@
+// To run a scala program just do scala filename.scala
+
+object ScalaTutorial{
+    def main(args: Array[String]){
+
+            var name = "Rajat Sethi"
+
+            for (i<-0 until name.length){
+                println(name(i))
+            }
+
+        }
+    }
---
+++ Data/scala/scala9.scala
@@ -0,0 +1,15 @@
+import scala.io.StdIn.{readLine, readInt}
+import scala.math._
+
+object ScalaTutorial{
+    def main(args: Array[String]){
+
+        val name = "Rajat"
+        val age = 10
+        val weight = 100
+
+        println(s"Hello $name")
+        println(f"I am ${age + 1} and weight $weight%.2f")
+
+    }
+}
